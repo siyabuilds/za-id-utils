18e7c1d3754bb62d828a670e9dad37222d68552f
Samson Lukhele <siyabonga.lukhele@umuzi.org>
2026-05-04 19:02:46 +0200

    feat: Export functions for external usage

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import parseIDNumber from "@/parse.js";
+import { getCitizenShip, calculateAge, parseDOB, getGender} from "@/dataCollector.js";
+
+export {
+    parseIDNumber,
+    getCitizenShip,
+    parseDOB,
+    calculateAge,
+    getGender,
+};
