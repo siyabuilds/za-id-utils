2192f09aa2cb44c38871b98d31dd4e93733ceda8
Samson Lukhele <siyabonga.lukhele@umuzi.org>
2026-05-04 18:53:44 +0200

    feat: complete SA ID parsing and validation pipeline

--- src/parse.ts
+++ src/parse.ts
@@ -1,3 +1,22 @@
 import { ParsedID } from "@/types/index.js";
+import {isLuhnValid} from "@/validateLuhnSequence.js";
+import {getCitizenShip, parseDOB, calculateAge, getGender} from "@/dataCollector.js";
 
-const parseIDNumber = (idNumber: string):ParsedID => {}
+const parseIDNumber = (idNumber: string):ParsedID => {
+    if (isLuhnValid(idNumber)) {
+        const dob = parseDOB(idNumber);
+        const age = calculateAge(dob);
+        return {
+            age: 0,
+            citizenship: getCitizenShip(idNumber),
+            dateOfBirth: dob,
+            gender: getGender(idNumber),
+            isValid: true
+        };
+    } else {
+        return {
+            isValid: false,
+            errors: [],
+        };
+    }
+}
