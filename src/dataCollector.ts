cfe15acf602e0d5930938d740dc333b7285982d7
Samson Lukhele <siyabonga.lukhele@umuzi.org>
2026-05-04 18:47:12 +0200

    feat: Add age calculation logic

--- src/dataCollector.ts
+++ src/dataCollector.ts
@@ -46,4 +46,21 @@ const parseDOB = (idNumber: string): Date => {
     return new Date(fullYear, month, day);
 };
 
-export { getCitizenShip, parseDOB };
+const calculateAge = (dateOfBirth: Date): number => {
+    const today = new Date();
+
+    let age = today.getFullYear() - dateOfBirth.getFullYear();
+
+    const hasHadBirthdayThisYear =
+        today.getMonth() > dateOfBirth.getMonth() ||
+        (today.getMonth() === dateOfBirth.getMonth() &&
+            today.getDate() >= dateOfBirth.getDate());
+
+    if (!hasHadBirthdayThisYear) {
+        age--;
+    }
+
+    return age;
+};
+
+export { getCitizenShip, parseDOB, calculateAge };
